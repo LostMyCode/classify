2d227f2d38d25c4cbe257dff5fa2a4dfce10562c
LostMyCode <studionyna@gmail.com>
2020-12-04 11:33:26 +0900

    Separated class creation things as ClassBuilder

---
+++ tools/ClassBuilder.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const classTmpl = fs.readFileSync("./templates/Class.json", "utf-8");
+
+class ClassBuilder {
+
+    constructor() {
+        this._class = JSON.parse(classTmpl);
+    }
+
+    setConstructor(_constructor) {
+        this._class.body.body[0].value.params = _constructor.params;
+        this._class.body.body[0].value.body = _constructor.body;
+    }
+
+    /**
+     * Add a method to class
+     * @param {Node} method Method AST
+     */
+    addMethod(method) {
+        this._class.body.body.push(method);
+    }
+
+    /**
+     * @return {Node} Class AST
+     */
+    getTree() {
+        return this._class;
+    }
+
+}
+
+module.exports = ClassBuilder;
